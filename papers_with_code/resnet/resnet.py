ce21f564b6c732b6cf2c4e5c4d2ce5482069d119
JJ <jeongjae1996@naver.com>
2023-03-26 15:15:47 +0900

    [feat] implement 'BasicBlock' class #8

--- papers_with_code/resnet/resnet.py
+++ papers_with_code/resnet/resnet.py
@@ -36,3 +36,44 @@ def conv1x1(
         stride=stride, 
         bias=False
     )
+
+class BasicBlock(nn.Module):
+    EXPANSION = 1
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        stride=1,
+        downsample=None,
+        norm_layer=nn.BatchNorm2d
+    ):
+        super().__init__()
+
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = norm_layer(out_channels)
+
+        self.conv2 = conv3x3(out_channels, out_channels, stride)
+        self.bn2 = norm_layer(out_channels)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        # shortcut/skip connection
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out += identity
+        out = self.relu(out)
+
+        return out
